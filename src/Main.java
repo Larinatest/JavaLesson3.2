287fcc1a3a552d3b5fb997c41139894e3ca1bcc6
Evgenia Larina <larsik29@yandex.ru>
2024-06-30 16:39:46 +0300

    README changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+
+        double heightM = 1.87;
+        double weightKg = 98;
+
+        int bmi = service.calculate(weightKg, heightM);
+
+        System.out.println("Рост: " + heightM + " м");
+        System.out.println("Вес: " + weightKg + " кг");
+        System.out.println("Индекс массы тела (BMI): " + bmi);
     }
-}
+    }
